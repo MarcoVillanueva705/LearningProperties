15b8a4e1ff8c18897b41f82f5e193a7f42b758c8
marco villanueva <villanuevamarco98@gmail.com>
2020-07-18 18:39:45 -0600

    Add project files.

---
+++ LearningProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LearningProperties
+{
+    class Player
+    {
+
+        public bool isAlive = true;
+        int health = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+
+        public void checkDeath()
+        {
+            if (health <= 0)
+                isAlive = false;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                player.checkDeath();
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+
