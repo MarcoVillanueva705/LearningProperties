59d30161d28b528c24a82942ce9b63a3b5eb7162
marco villanueva <villanuevamarco98@gmail.com>
2020-07-18 22:35:22 -0600

    properties exercise

--- LearningProperties/Program.cs
+++ LearningProperties/Program.cs
@@ -4,8 +4,17 @@ namespace LearningProperties
 {
     class Player
     {
-
-        public bool isAlive = true;
+        //properties are members that provide
+        //mechanisms to read, write and compute
+        //the value of a private field
+        public bool IsAlive
+        {
+            //properties allow more control over access
+            get
+            {
+                return health > 0;
+            }
+        }
         int health = 100;
 
         public void Hit()
@@ -14,11 +23,6 @@ namespace LearningProperties
             health -= r.Next(5, 50);
         }
 
-        public void checkDeath()
-        {
-            if (health <= 0)
-                isAlive = false;
-        }
         
     }
     class Program
@@ -26,11 +30,15 @@ namespace LearningProperties
         static void Main(string[] args)
         {
             Player player = new Player();
+            //can't do this because property is read-only
+            //properties great way to embed functionality 
+            //into getters and setters
+            //player.IsAlive = false;
+
             for (int i = 0; i < 20; i++)
             {
                 player.Hit();
-                player.checkDeath();
-                Console.WriteLine("");
+                Console.WriteLine("Is player alive" + player.IsAlive);
             }
         }
     }
